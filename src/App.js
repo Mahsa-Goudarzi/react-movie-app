e61a0db532a1d4f7d27c8afb03a4342be709fef7
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-04-26 21:54:39 +0430

    using async await instead of a .then chain

--- src/App.js
+++ src/App.js
@@ -6,22 +6,18 @@ import "./App.css";
 export default function App() {
   const [movies, setMovies] = useState([]);
 
-  function handleFetchMovies() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function handleFetchMovies() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
