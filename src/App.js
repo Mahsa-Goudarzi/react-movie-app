021bee06d3e0453f852682eeaecbbacbe61952d1
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-04-26 23:03:08 +0430

    render the proper message in a cleaner way

--- src/App.js
+++ src/App.js
@@ -34,17 +34,25 @@ export default function App() {
     setIsLoading(false);
   }
 
+  let content = <p>Found no movies</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={handleFetchMovies}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && <p>Found no movies</p>}
-        {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
